463c804aca98cfdbbd1429e961b316d033b4ae7b
totti999 <tottialfarabi@mhs.mdp.ac.id>
2023-05-05 14:12:03 +0700

    update

--- Universitas/app/Http/Controllers/ProdiController.php
+++ Universitas/app/Http/Controllers/ProdiController.php
@@ -4,43 +4,44 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Prodi;
+use App\Models\Fakultas;
 use Illuminate\Http\Request;
 
 class ProdiController extends Controller
 {
     public function index(){
         $prodi = Prodi::all();
-      
+
         return view('prodi.index')->with('prodis', $prodi);
     }
 
        public function create()
     {
-        return view('prodi.create');
+        $fakultas = Fakultas::all();
+        return view('prodi.create', compact('fakultas'));
     }
 
     public function store(Request $request)
     {
-        // dd($request); for all 
+        // dd($request); for all
         // dd($request->nama_fakultas); for specify
 
         $validasi = $request->validate([
 
              'nama_prodi'=> 'required',
             'fakultas_id' => 'required',
-          
+
 
         ]);
 
         // dd($validasi);
 
         $prodi = new Prodi();
-        $prodi->nama_prodi = $validasi['nama_fakultas'];
+        $prodi->nama_prodi = $validasi['nama_prodi'];
         $prodi->fakultas_id = $validasi['fakultas_id'];
-      
         $prodi->save();
 
-        return redirect()->route('prodi.index')->with('success',"Data".$validasi['nama_fakultas']. "berhasil disimpan");
+        return redirect()->route('prodi.index')->with('success',"Data".$validasi['nama_prodi']. "berhasil disimpan");
 
     }
 }
--- Universitas/resources/views/prodi/create.blade.php
+++ Universitas/resources/views/prodi/create.blade.php
@@ -11,7 +11,7 @@
                 <div class="card-body">
                   <h4 class="card-title">Tambah Prodi</h4>
                   <p class="card-description">
-                    Formulir 
+                    Formulir
                   </p>
                   <form class="forms-sample" action="{{ route('prodi.store')}} " method="post">
                     @csrf
@@ -23,13 +23,17 @@
                       @enderror
                     </div>
                     <div class="form-group">
-                      <label for="fakultas_id">Pilih Fakult Prodi</label>
-                      <input type="text" class="form-control" name="nama_fakultas" placeholder="nama fakultas">
+                      <label for="fakultas_id">Pilih Fakultas Prodi</label>
+                     <select name="fakultas_id" class="form-control">
+                     @foreach ($fakultas as $item)
+                            <option value="{{$item->id}}">{{$item->nama_fakultas}}</option>
+                     @endforeach
+                    </select>
                       @error('nama_fakultas')
                          <span class="text-danger">{{$message}}</span>
                       @enderror
                     </div>
-                     
+
                     <button type="submit" class="btn  btn-rounded btn-success me-2">Simpan</button>
                     <a  href="{{route('fakultas.index')}}" class="btn btn-rounded btn-dark">batal</a>
                   </form>
@@ -37,5 +41,5 @@
               </div>
             </div>
 </div>
-    
+
 @endsection
